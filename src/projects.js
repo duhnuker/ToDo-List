5b0b8b8bbe3691929437d73b646249813b00f968
duhnuker <dinukanh@gmail.com>
2023-06-02 14:54:53 +1000

    createToDo function

--- src/projects.js
+++ src/projects.js
@@ -1,23 +1,16 @@
 import './todo.js'
 import { populateStorage } from './local-storage'; 
 
-let allProjectContainer = (function() {
-
-    let defaultProject = new Project('Default');
-    let projects = [defaultProject];
-
-    return {
-        defaultProject,
-        projects,
-    }
-})();
-
 export default class Project {
     constructor(name, id) {
         this.name = name;
         this.id = id;
         this.toDos = [];
     }
+}
+
+function createProject(name) {
+    allToDoContainer.projects.push(new Project(name));
 }
 
 
--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,16 @@
 import Project from "./projects";
 
+export let allToDoContainer = (function() {
+
+    let defaultProjectFolder = new Project('Default');
+    let projects = [defaultProject];
+
+    return {
+        defaultProjectFolder,
+        projects,
+    }
+})();
+
 export class ToDo {
     constructor(name, priority, isCompleted, date) {
         this.name = name;
@@ -7,4 +18,24 @@ export class ToDo {
         this.isCompleted = isCompleted;
         this.date = date;
     }
+}
+
+function createToDo(
+    name, 
+    priority, 
+    isCompleted, 
+    date,
+    insertToProject = ''
+    ) {
+        //if no project name insert to defaultProjectFolder
+    let todo = new ToDo(name, priority, isCompleted, date);
+    if (insertToProject === '') {
+        allToDoContainer.defaultProjectFolder.push(todo);
+    }
+    allToDoContainer.projects.forEach((currentProject) => {
+        if (currentProject.name === insertToProject) {
+            allToDoContainer.defaultProjectFolder.push(todo)
+            currentProject.projects.push(todo)
+        }
+    })
 }
