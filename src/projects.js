04e9fd3789d73274a5a4261fbcd05d347bd1d005
duhnuker <dinukanh@gmail.com>
2023-06-02 13:01:31 +1000

    allProjectContainer established

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,17 @@
 import './todo.js'
 import { populateStorage } from './local-storage'; 
 
+let allProjectContainer = (function() {
+
+    let defaultProject = new Project('Default');
+    let projects = [defaultProject];
+
+    return {
+        defaultProject,
+        projects,
+    }
+})();
+
 export default class Project {
     constructor(name, id) {
         this.name = name;
