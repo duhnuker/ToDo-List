f8aa037f4c3927c0558a214f62eca5241e49903c
Dinuka <dinukanh@gmail.com>
2023-06-13 16:13:21 +1000

    all test sequences working console log format

--- src/index.js
+++ src/index.js
@@ -43,13 +43,21 @@ let todo2 = new ToDo('Clean the kitchen', 'Medium', 'Not Complete', '09/06/2023'
 let todo3 = new ToDo('Wipe down the tv screen', 'Low', 'Not Complete', '09/06/2023');
 let todo4 = new ToDo('Take out the trash', 'High', 'Not Complete', '09/06/2023');
 
+//Needs to by default be added to allToDoContainer
+globalContainer.allProjects[0].toDoArray.push(todo2, todo3, todo4);
+//Then is added to Cleaning Project
 globalContainer.allProjects[3].toDoArray.push(todo2, todo3, todo4);
 
-//s6 showToDo()
+//s6
+
+console.log(globalContainer.allProjects[3].toDoArray[1], 's6');
 
 //s7
-// delete ToDo with splice()
+//remove from allToDoContainer
+console.log(globalContainer.allProjects[0].toDoArray.splice(1,1), 's7');
+//remove from Cleaning toDoArray
+console.log(globalContainer.allProjects[3].toDoArray.splice(0,1), 's7');
 
-//s8 deleteFolder('ToDo1,ToDo2') use array.length = 0;
+//s8
 
-
+console.log(globalContainer.allProjects.splice(1,1), 's8'); //position one, 1 item removed//
