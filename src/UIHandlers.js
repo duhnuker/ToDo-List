ebb33987308345190c0bc438191d1ea8476e4488
Dinuka <dinukanh@gmail.com>
2023-08-15 23:10:08 +1000

    Plan Update / For my own understanding

--- src/UIHandlers.js
+++ src/UIHandlers.js
@@ -1,23 +1,33 @@
-//include tab loading (day, week month)
 
-function defaultDisplay() {
-
+function loadPage() {
+    //default loads onto All tab
+    //display  allToDoContainer Project as default load
+    //display All other Projects on the left
 }
 
 function displayTodayTab() {
-
+    document.querySelector('#todayButton').addEventListener('click', function(event) { 
+    //select button tab
+    //on-click using date-fns updates content-container to display
+    //ToDo's relevant by date load.
+    } )
 }
 
 function displayWeekTab() {
-
+    document.querySelector('#weekButton').addEventListener('click', function(event) {
+    } )
 }
 
 function displayMonthTab() {
-
+    document.querySelector('#monthButton').addEventListener('click', function(event) {
+    } )
 }
 
-function displayProjectForm() {
+function createProjectForm() {
     document.querySelector('#addProject').addEventListener('click', function(event) {
-    //create form
+    //button is pressed, input box appears
+    //take name from user
+    //option to finalise, or cancel form
+    //using <li> it is then placed under 'Projects Subheading'
     } )
 }
