eabb39585b84257a6bb1ba12693d763d36b5652e
Dinuka <dinukanh@gmail.com>
2023-10-18 22:57:38 +1100

    todoList module basic function layout

---
+++ src/todoList.js
@@ -0,0 +1,30 @@
+export default class ToDoList {
+    constructor() {
+        this.projects = [];
+
+    }
+
+    // ToDoList functions
+
+setProjects(projects) {
+
+}
+
+getProjects() {
+
+}
+
+getSpecificProject(projectName) {
+
+}
+
+addProject(newProjectName) {
+
+}
+
+deleteProject(projectName) {
+    
+}
+
+}
+
