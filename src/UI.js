d45963bbce1dbc6f2ef6d3b98504e8ec344eb501
Dinuka <dinukanh@gmail.com>
2023-10-23 23:20:38 +1100

    UI function implementation started

---
+++ src/UI.js
@@ -0,0 +1,40 @@
+export default class UI {
+    
+    static loadHomepage() {
+
+    }
+
+    static loadProjects() {
+
+    }
+
+    static loadToDos(projectName) {
+        //Selected Project's ToDOs to load UI 
+    }
+
+    static loadProjectContent(projectName) {
+
+    }
+
+    static createProject(name) {
+        //innerHtml for Project UI
+    }
+
+    static createToDo(name, dueDate) {
+        ////innerHtml for Project UI
+    }
+
+
+    static clear() {
+
+    }
+
+    static clearProjects() {
+
+    }
+
+    static clearToDos() {
+        
+    }
+
+}
